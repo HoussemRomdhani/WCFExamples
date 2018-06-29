0ee8e617926a1bd5d0a96c86598cc50d57efafd4
Houssem Romdhani <romdhanihoussem77@gmail.com>
2018-06-29 20:52:33 +0200

    set up contracts (data & service)

---
+++ wcfExamples.Contracts/BookData.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace wcfExamples.Contracts
+{
+    [DataContract]
+    public class BookData
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Author { get; set; }
+
+        [DataMember]
+        public decimal Pricec { get; set; }
+    }
+}
---
+++ wcfExamples.Contracts/IBookService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace wcfExamples.Contracts
+{
+    [ServiceContract]
+    public interface IBookService
+    {
+        [OperationContract]
+        BookData GetById(int id);
+
+        [OperationContract]
+        IEnumerable<BookData> GetAll();
+
+        [OperationContract]
+        void Add(BookData book);
+
+        [OperationContract]
+        void Update(BookData book);
+
+        [OperationContract]
+        void Delete(int id);
+    }
+}
