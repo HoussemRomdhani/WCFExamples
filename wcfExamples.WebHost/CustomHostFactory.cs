ec18a81e572a6810d06b3a6c1cde5fd968e4fa93
Houssem Romdhani <romdhanihoussem77@gmail.com>
2018-06-30 12:45:47 +0200

    add a web host

---
+++ wcfExamples.WebHost/CustomHostFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace wcfExamples.WebHost
+{
+    public class CustomHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            ServiceHost host = new ServiceHost(serviceType, baseAddresses);
+            return host;
+        }
+    }
+}
